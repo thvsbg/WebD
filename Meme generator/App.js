9c15615ef0c3986f983081459c31d408b7e6b284
thvsbg <130590871+thvsbg@users.noreply.github.com>
2023-05-19 19:27:38 +0530

    Add files via upload

---
+++ Meme generator/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './Header';
+import Meme from './Meme';
+
+export default function App() {
+  return (
+   <div>
+    <Header />
+    <Meme />
+
+
+
+
+   </div>
+   
+  );
+}
+
+
